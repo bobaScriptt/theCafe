26078da5d8f6a9f37421f4287d5b74fc78a279a8
GRAY <158392843+bobaScriptt@users.noreply.github.com>
2024-02-01 21:28:12 -0500

    V1.0.7
    -- Adding down arrow indicator under welcome screen.
    -- Implemented scroll snapping js logic, but not utilized at the moment. The website is too small for it's use. Will be used as website expands.

---
+++ backend/logic.js
@@ -0,0 +1,16 @@
+// let sectionIds = ["hero", "section1", "section2"];
+// let currentSectionIndex = 0;
+
+// window.addEventListener('wheel', (event) => {
+//   event.preventDefault();
+
+//   if (event.deltaY > 0) {
+//     // Scrolling down
+//     currentSectionIndex = Math.min(currentSectionIndex + 1, sectionIds.length - 1);
+//   } else {
+//     // Scrolling up
+//     currentSectionIndex = Math.max(currentSectionIndex - 1, 0);
+//   }
+
+//   document.getElementById(sectionIds[currentSectionIndex]).scrollIntoView({behavior: 'smooth'});
+// }, {passive: false});
